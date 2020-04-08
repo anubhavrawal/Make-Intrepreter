ad5fad2a2161c2090733b2be6785b5c7dadc0e33
Altan Rawal <detroid.stan@gmail.com>
2020-04-07 21:21:32 -0400

    Version 0.2

--- fake.c
+++ fake.c
@@ -1,8 +1,12 @@
 #include <errno.h>
 #include "fake.h"
 
-void rule(recipe_t ** pointers_to_recipes, char *buf, int line){
+recipe_t ** rule(recipe_t ** pointers_to_recipes, char *buf, int line){
 	char *rule_target;
+	pointers_to_recipes = realloc(pointers_to_recipes, (line + 1) *sizeof(recipe_t*));
+	pointers_to_recipes[line] = malloc(sizeof(recipe_t));
+	pointers_to_recipes[line]->deps  = malloc(sizeof(char *));
+	//pointers_to_recipes[line]->deps[0] = malloc(sizeof(char *));
 	
 	rule_target = strsep(&buf, ":");
 
@@ -19,11 +23,14 @@ void rule(recipe_t ** pointers_to_recipes, char *buf, int line){
 			if (deps == NULL){
 				break;
 			}
-			pointers_to_recipes[line]->deps = realloc(pointers_to_recipes[line]->deps, (count+1) *sizeof(char *));
+			pointers_to_recipes[line]->deps = realloc(pointers_to_recipes[line]->deps, (count+2) *sizeof(char *));
 			pointers_to_recipes[line]->deps[count] = strdup(deps);
 			printf("The deps are: %s \n",pointers_to_recipes[line]->deps[count]);
-		}	
+		}
+
+		pointers_to_recipes[line]->deps[count+1] = NULL;	
 	}
+	return pointers_to_recipes;
 
 }
 
@@ -31,9 +38,11 @@ void cmds(recipe_t ** pointers_to_recipes, char *buf, int line, int count){
 	char * tmp;
 	tmp = strsep(&buf, "\t");
 
-	pointers_to_recipes[line]->commands = realloc(pointers_to_recipes[line]->commands, (count+1) *sizeof(char *));
+	pointers_to_recipes[line]->commands = realloc(pointers_to_recipes[line]->commands, (count+2) *sizeof(char *));
 	pointers_to_recipes[line]->commands[count] = strdup(buf);
-	printf("The commands are: %s \n",pointers_to_recipes[line]->commands[count]);
+
+	pointers_to_recipes[line]->commands[count+1] = NULL;
+	printf("The commands %d are: %s \n",line, pointers_to_recipes[line]->commands[count]);
 
 }
 
@@ -46,7 +55,7 @@ int main(){
 	parser state = RULE_START;
     
     //Pointers of Pointer for storing all the cards
-    recipe_t **pointers_to_recipes; //= malloc(sizeof(recipe_t*));
+    recipe_t **pointers_to_recipes = malloc(sizeof(recipe_t*));
     //pointers_to_recipes[0] = malloc(sizeof(recipe_t));
 
     if (fake_reader == NULL) 
@@ -70,22 +79,20 @@ int main(){
 		if (*reader == '\n'){
             state = RULE_START;
 			line++;
-            continue;
+			continue;
         }
 
         buf = reader;
         
         if (state == RULE_START){
-			pointers_to_recipes = realloc(pointers_to_recipes, (line + 1) *sizeof(recipe_t*));
-			pointers_to_recipes[line] = malloc(sizeof(recipe_t));
-
-            rule(pointers_to_recipes, buf, line);
+            pointers_to_recipes = rule(pointers_to_recipes, buf, line);
             state = COMMANDS;
 			cmd_track = 0;
+			pointers_to_recipes[line]->commands = calloc(sizeof(char *), 1);
 			continue;
         }
 
-		/*
+		
 		if (state == COMMANDS){
 			if (*reader == '\t'){
 				cmds(pointers_to_recipes, buf, line, cmd_track);
@@ -99,10 +106,40 @@ int main(){
 			}
 			
 		}
-		*/
+		
 
     }
-    
 
+	free(reader);
+	int i;
+	for (i=0; i<=line; i++){
+		free(pointers_to_recipes[i]->target);
+		int count = 0;
+		int tmp = sizeof(&*pointers_to_recipes[i]->deps)/ sizeof(*pointers_to_recipes[i]->deps[0]);
+
+		while (pointers_to_recipes[i]->deps[count] != NULL){
+			free(pointers_to_recipes[i]->deps[count]);
+			count++;
+		}
+		free(pointers_to_recipes[i]->deps[count]);
+		free(pointers_to_recipes[i]->deps);
+		
+		count = 0;
+		while (pointers_to_recipes[i]->commands[count] != NULL){
+			free(pointers_to_recipes[i]->commands[count]);
+			count++;
+		}
+		free(pointers_to_recipes[i]->commands[count]);
+		free(pointers_to_recipes[i]->commands);
+
+		free(pointers_to_recipes[i]);
+	}
+	printf("Hello!!");
+	//free(pointers_to_recipes[i]->deps);
+	//free(pointers_to_recipes[i]->commands);
+	free(pointers_to_recipes[i]);
+	free(pointers_to_recipes);
+	
     fclose(fake_reader); 
+
 }
