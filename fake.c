8b83d53d475e7c778ba6eb1486c40b47e92052e1
Altan Rawal <detroid.stan@gmail.com>
2020-04-09 18:44:44 -0400

    Version 0.4

--- fake.c
+++ fake.c
@@ -1,22 +1,22 @@
 #include <errno.h>
 #include "fake.h"
 
-recipe_t ** rule(recipe_t ** pointers_to_recipes, char *buf, int line){
+int rule(recipe_t ** pointers_to_recipes, char *buf, int line){
 	char *rule_target;
-	pointers_to_recipes = realloc(pointers_to_recipes, (line + 1) *sizeof(recipe_t*));
-	pointers_to_recipes[line] = calloc(1, sizeof(recipe_t));
-	pointers_to_recipes[line]->deps  = malloc(sizeof(char *));
+	
 	//pointers_to_recipes[line]->deps[0] = malloc(sizeof(char *));
+
+	if (strchr(buf, ':') == NULL){
+		return -1;
+	}
 	
 	rule_target = strsep(&buf, ":");
 
-
 	if (strcmp(rule_target,buf)){
 		pointers_to_recipes[line]->target = strdup(rule_target);
-		
-		printf("The Targets are: %s \n",pointers_to_recipes[line]->target);
 
 		char *deps = strsep(&buf, " ");
+
 		int count =0;
 		while (deps){
             deps = strsep(&buf, " ");
@@ -24,27 +24,63 @@ recipe_t ** rule(recipe_t ** pointers_to_recipes, char *buf, int line){
 				break;
 			}
 			pointers_to_recipes[line]->deps = realloc(pointers_to_recipes[line]->deps, (count+1) *sizeof(char *));
+			
+			
 			pointers_to_recipes[line]->deps[count] = strdup(deps);
 			
-			printf("The deps are: %s \n",pointers_to_recipes[line]->deps[count]);
+			
+			
 			count ++;
 		}
 		pointers_to_recipes[line]->dep_count = count;
 
 	}
-	return pointers_to_recipes;
-
+	else
+	{
+		return -1;
+	}
+	
+	
+	return 1;
 }
 
-void cmds(recipe_t ** pointers_to_recipes, char *buf, int line, int count){
+void display(recipe_t ** pointers_to_recipes, int line){
+	int i;
+	int count;
+	for (i=0; i<=line; i++){
+		printf("The Targets are: %s \n",pointers_to_recipes[i]->target);
+
+		for(count=0; count < pointers_to_recipes[i]->dep_count; count++){
+			printf("The %s's  deps %d is: %s \n",pointers_to_recipes[i]->target, count,pointers_to_recipes[i]->deps[count]);
+		}
+		
+		for(count=0; count < pointers_to_recipes[i]->cmd_count; count++){
+			printf("The %s's commands %d is: %s \n", pointers_to_recipes[i]->target, count , pointers_to_recipes[i]->commands[count]);
+		}
+		printf("\n");
+	}
+}
+
+int  cmds(recipe_t ** pointers_to_recipes, char *buf, int line, int count){
 	char * tmp;
+
 	tmp = strsep(&buf, "\t");
 
-	pointers_to_recipes[line]->commands = realloc(pointers_to_recipes[line]->commands, (count+2) *sizeof(char *));
-	pointers_to_recipes[line]->commands[count] = buf;
+	if (strcmp(tmp,buf) == 0){
+		return -1;
+	}
 
-	pointers_to_recipes[line]->commands[count+1] = NULL;
-	printf("The commands %d are: %s \n", line , pointers_to_recipes[line]->commands[count]);
+	if (strchr(buf, ':') != NULL){
+		return -1;
+	}
+
+	int length_cmd = strlen(buf);
+	pointers_to_recipes[line]->commands = realloc(pointers_to_recipes[line]->commands, (count+1) *sizeof(char *));
+	pointers_to_recipes[line]->commands[count] = strndup(buf, length_cmd -1);
+	
+	pointers_to_recipes[line]->commands[count][length_cmd -1] = '\0';
+
+	return 1;
 
 }
 
@@ -55,10 +91,13 @@ int main(){
     int line = 0;
 	int cmd_track;
 	parser state = RULE_START;
+	int condition = 1;
+
+	int validation_check = 0;
     
     //Pointers of Pointer for storing all the cards
     recipe_t **pointers_to_recipes = malloc(sizeof(recipe_t*));
-    
+    //pointers_to_recipes[0] = malloc(sizeof(recipe_t));
 
     if (fake_reader == NULL) 
     { 
@@ -66,61 +105,100 @@ int main(){
         return 0; 
     }
     
-    while(1){
-		fgets(reader, 1024, fake_reader);
-			
-		if (feof(fake_reader)){
-			break;
-		}
-        
-		//printf("line: %d \n", line);
-        if (*reader == '#'){
+    while( (condition == 1) && (fgets(reader, 1024, fake_reader)) ){
+
+		if (*reader == '#'){
             continue;
         }
+		buf = reader;
 
-		if ( (*reader == '\n') && (state == COMMANDS)){
-            state = RULE_START;
-			line++;
-			continue;
-        }
+		switch (state){
+			case COMMANDS:
+				if (*reader == '\n'){
+					line++;
+					state = RULE_START;
+				}
 
-		if ((*reader == '\n') || (reader == NULL)){
-			continue;
-		}
+				else if (*reader == '\t'){
+					validation_check = 0;
+					validation_check = cmds(pointers_to_recipes, buf, line, cmd_track);
 
-        buf = reader;
-        
-        if (state == RULE_START){
-            pointers_to_recipes = rule(pointers_to_recipes, buf, line);
-            state = COMMANDS;
-			cmd_track = 0;
-			pointers_to_recipes[line]->commands = calloc(1,sizeof(char *));
-			continue;
-        }
+					if (validation_check == -1){
+						printf("Invalid format!!!! CMD state\n");
+						condition = 0;
+						break;
+					}
+					cmd_track++;
+					pointers_to_recipes[line]->cmd_count = cmd_track;
+				}
 
-		
-		if (state == COMMANDS){
-			if (*reader == '\t'){
-				cmds(pointers_to_recipes, buf, line, cmd_track);
-				pointers_to_recipes[line]->cmd_count = cmd_track;
-				cmd_track++;
-				continue;
-			}
+				else{
+					printf("Error!!!! in file format!!! \n");
+					condition = 0;
+					break;
+				}
 
-			else{
-				printf("Error!!!! in file format!!!");
 				break;
-			}
 			
+			case RULE_START:
+				if ((*reader == '\t') || (*reader =='\n') || (reader == NULL)){
+					break;
+				}
+				pointers_to_recipes = realloc(pointers_to_recipes, (line + 1) *sizeof(recipe_t*));
+				pointers_to_recipes[line] = calloc(1, sizeof(recipe_t));
+				pointers_to_recipes[line]->deps  = malloc(sizeof(char *));
+
+				//Creating a tmp memory to get rid of \n while reading the reciepe
+				//---------------------------------------------
+				char *tmp;
+				int len_buf = strlen(buf);
+				tmp = strndup(buf, len_buf-1); 
+				tmp[len_buf-1] = '\0';
+				//-----------------------------------------
+
+				validation_check = 0;	
+				validation_check = rule(pointers_to_recipes, tmp, line);
+				
+				free(tmp);// freeing the temporary located memory
+
+				//if -1 falgged by check then,
+				if (validation_check == -1){
+					line++;
+					printf("Invalid format rule state!!!! \n");
+					condition = 0; //breaking the while loop
+					break;
+				}
+				
+
+				state = COMMANDS;
+				cmd_track = 0;
+				pointers_to_recipes[line]->commands = calloc(1,sizeof(char *));
+				continue;
+
+				break;
+
+			default:
+				break;
 		}
-		
 
     }
+	fclose(fake_reader);
+
+	//If there is just extra spaces with no target or commands
+	//Then decrement total number of lines as the last incerement was unnecessary.
+	if (state == RULE_START){
+		line--;
+	}
+
+	//Display only if eveything went well
+	if (condition){
+		display(pointers_to_recipes, line);
+	}
 
 	//Freeing all the used memory
 	free(reader);
 	int i;
-	for (i=0; i<line; i++){
+	for (i=0; i<=line; i++){
 		free(pointers_to_recipes[i]->target);
 
 		int count;
@@ -129,16 +207,13 @@ int main(){
 		}
 		free(pointers_to_recipes[i]->deps);
 		
-		for(count=0; i < pointers_to_recipes[i]->cmd_count; count++){
+		for(count=0; count < pointers_to_recipes[i]->cmd_count; count++){
 			free(pointers_to_recipes[i]->commands[count]);
-			count++;
 		}
 		free(pointers_to_recipes[i]->commands);
 
 		free(pointers_to_recipes[i]);
 	}
 	free(pointers_to_recipes);
-	
-    fclose(fake_reader); 
 
 }
--- tmp.c
+++ tmp.c
@@ -1,6 +1,29 @@
-if(cmd_track > 0){
-				pointers_to_recipes[line]->commands[cmd_track+1] = NULL;
-			}
-			else{
-				pointers_to_recipes[line]->commands[cmd_track] = NULL;
-			}
+//printf("line: %d \n", line);
+        
+		/*
+		if ( (*reader == '\n') && (state == COMMANDS)){
+            state = RULE_START;
+			line++;
+			continue;
+        }
+
+		if ((*reader == '\n') || (reader == NULL)){
+			continue;
+		}
+
+        buf = reader;
+        
+        if (state == RULE_START){
+            pointers_to_recipes = rule(pointers_to_recipes, buf, line);
+            state = COMMANDS;
+			cmd_track = 0;
+			pointers_to_recipes[line]->commands = calloc(1,sizeof(char *));
+			continue;
+        }
+
+		
+		if (state == COMMANDS){
+			
+			
+		}
+		*/
